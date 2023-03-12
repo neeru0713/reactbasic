2417e1167fcfcc79bc247331a876b86a69fe1c44
ritish73 <rgupta10_be18@thapar.edu>
2023-03-12 12:30:02 +0530

    fixed main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import UserDashboard from './UserDashboard.jsx'
+import UserDashboard from './session1-takehome/UserDashboard.jsx'
 
 export default function App() {
   return (
--- src/session1-takehome/UserDashboard.jsx
+++ src/session1-takehome/UserDashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from "react-dom/client";
-import './index.css'
+import '../index.css'
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
